4b06b5ea1000bca20b3b5824647002965229b39d
Akshat Tandon <akshat.thewayoflife@gmail.com>
2019-03-04 19:05:59 +0530

    changes made

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import Header from './Header';
+import HelloUA from '../components/helloUA'
 const Index = () => (
   <div>
+  <Header/>
+  <HelloUA/>
+  <div>
     <p>Hello Next.js</p>
+  </div>
   </div>
 )
 
